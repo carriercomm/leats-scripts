c7f88a0b5d590062327a6f023e542bf2f2382d95
gruberrichard <gruberrichard@gmail.com>
2013-05-31 13:26:39 +0200

    Create ALTS Release

---
+++ CreateRelease.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Creating Release.."
+D=`date +%Y%m%d`;
+echo "DATE: $D"
+
+tar -czvf /ALTS_${D}.tgz /var/www/cgi-bin /var/www/html/ALTSicons /var/www/html/authorized_keys /etc/motd /etc/httpd/conf/httpd.conf /ALTS/activate /ALTS/ALTSLogin /ALTS/ALTSLogout /ALTS/EXERCISES /ALTS/lib /ALTS/REINSTALL /ALTS/RESET /ALTS/SECURITY/ALTSkey /ALTS/SECURITY/id_rsa /ALTS/Settings.alts
